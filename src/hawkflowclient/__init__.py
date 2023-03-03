4c8e04550b95c0c5f202db142a6d6c54b7702b8c
felbus <paul.felby@gmail.com>
2023-03-03 15:41:30 +0000

    improv logging

--- src/hawkflowclient/__init__.py
+++ src/hawkflowclient/__init__.py
@@ -1,1 +1,9 @@
+import sys
+import logging
+from logging import NullHandler
+
 from .hawkflow_decorators import HawkflowTimed, HawkflowMetrics, HawkflowException
+
+
+logging.getLogger(__name__).addHandler(NullHandler())
+
--- src/hawkflowclient/hawkflow_api.py
+++ src/hawkflowclient/hawkflow_api.py
@@ -17,27 +17,39 @@ class HawkflowAPI:
         self.wait_time = wait_time
 
     def metrics(self, process: str, meta: str = "", items=None):
-        if items is None:
-            items = []
+        try:
+            if items is None:
+                items = []
 
-        url = f"{_hawkflow_api_url}/metrics"
-        data = _metric_data(process, meta, items)
-        return self._hawkflow_post(url, data)
+            url = f"{_hawkflow_api_url}/metrics"
+            data = _metric_data(process, meta, items)
+            return self._hawkflow_post(url, data)
+        except HawkFlowException as he:
+            logging.error(f"HawkFlowException - {str(he)}")
 
     def exception(self, process: str, meta: str = "", exception_text: str = ""):
-        url = f"{_hawkflow_api_url}/exception"
-        data = _exception_data(process, meta, exception_text)
-        return self._hawkflow_post(url, data)
+        try:
+            url = f"{_hawkflow_api_url}/exception"
+            data = _exception_data(process, meta, exception_text)
+            return self._hawkflow_post(url, data)
+        except HawkFlowException as he:
+            logging.error(f"HawkFlowException - {str(he)}")
 
     def start(self, process: str, meta: str = "", uid: str = ""):
-        url = f"{_hawkflow_api_url}/start"
-        data = _timed_data(process, meta, uid)
-        return self._hawkflow_post(url, data)
+        try:
+            url = f"{_hawkflow_api_url}/start"
+            data = _timed_data(process, meta, uid)
+            return self._hawkflow_post(url, data)
+        except HawkFlowException as he:
+            logging.error(f"HawkFlowException - {str(he)}")
 
     def end(self, process: str, meta: str = "", uid: str = ""):
-        url = f"{_hawkflow_api_url}/end"
-        data = _timed_data(process, meta, uid)
-        return self._hawkflow_post(url, data)
+        try:
+            url = f"{_hawkflow_api_url}/end"
+            data = _timed_data(process, meta, uid)
+            return self._hawkflow_post(url, data)
+        except HawkFlowException as he:
+            logging.error(f"HawkFlowException - {str(he)}")
 
     def _hawkflow_post(self, url, data):
         try:
@@ -60,6 +72,4 @@ class HawkflowAPI:
                     retries = retries + 1
                     logging.error(f"HawkFlow api failed on attempt {retries} - {traceback.format_exc()}")
         except HawkFlowException as he:
-            logging.error(f"HawkFlow Exception - {he.message}")
-        except Exception as err:
-            logging.error(f"HawkFlow Exception - {traceback.format_exc()}")
+            logging.error(f"HawkFlowException - {str(he)}")
